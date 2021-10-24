aaefb6775c7021c536ea5a944fe48f247def6459
Marcello Esposito <esposito.marce@gmail.com>
2021-10-24 15:02:24 +0200

    test refactoring
    setUp() method is used to instantiate Alarm class.
    An `a` variable is defined in the class and used by all the test methods.

--- test_alarm.py
+++ test_alarm.py
@@ -3,81 +3,72 @@ from alarm import *
 
 class MyFirstTests(unittest.TestCase):
 
+    def setUp(self):
+        self.a = Alarm()
+
     def test_alarmIsOffAfterCreation(self):
-        a = Alarm()
-        self.assertFalse(a.isActive())
+        self.assertFalse(self.a.isActive())
     
     def test_alarmIsNotRingingAfterCreation(self):
-        a = Alarm()
-        self.assertFalse(a.isRinging())
+        self.assertFalse(self.a.isRinging())
 
     def test_offAlarmDetectingMotionDoesNotRing(self):
-        a = Alarm()
-        a.deactivate()
-        a.motionDetected()
-        self.assertFalse(a.isRinging())
+
+        self.a.deactivate()
+        self.a.motionDetected()
+        self.assertFalse(self.a.isRinging())
 
     def test_onAlarmDetectingMotionDoesRing(self):
-        a = Alarm()
-        a.activate()
-        a.motionDetected()
-        self.assertTrue(a.isRinging())
+        self.a.activate()
+        self.a.motionDetected()
+        self.assertTrue(self.a.isRinging())
 
     def test_alarmDoesNotRingAfter_Activation_MotionDetection_Reset(self):
-        a = Alarm()
-        a.activate()
-        a.motionDetected()
-        a.reset()
-        self.assertFalse(a.isRinging())
+        self.a.activate()
+        self.a.motionDetected()
+        self.a.reset()
+        self.assertFalse(self.a.isRinging())
 
     def test_alarmIsOnAfter_Activation_MotionDetection_Reset(self):
-        a = Alarm()
-        a.activate()
-        a.motionDetected()
-        a.reset()
-        self.assertTrue(a.isActive())
+        self.a.activate()
+        self.a.motionDetected()
+        self.a.reset()
+        self.assertTrue(self.a.isActive())
 
     def test_alarmIsOffAfter_Activation_Deactivation(self):
-        a = Alarm()
-        a.activate()
-        a.deactivate()
-        self.assertFalse(a.isActive())
+        self.a.activate()
+        self.a.deactivate()
+        self.assertFalse(self.a.isActive())
     
     def test_alarmIsOffAfter_Activation_MotionDetection_Deactivation(self):
-        a = Alarm()
-        a.activate()
-        a.motionDetected()
-        a.deactivate()
-        self.assertFalse(a.isActive())
+        self.a.activate()
+        self.a.motionDetected()
+        self.a.deactivate()
+        self.assertFalse(self.a.isActive())
 
     def test_alarmDoesNotRingAfter_Activation_MotionDetection_Deactivation(self):
-        a = Alarm()
-        a.activate()
-        a.motionDetected()
-        a.deactivate()
-        self.assertFalse(a.isRinging())
+        self.a.activate()
+        self.a.motionDetected()
+        self.a.deactivate()
+        self.assertFalse(self.a.isRinging())
 
     def test_alarmDoesRingAfter_Activation_MotionDetection_Reset_MotionDetection(self):
-        a = Alarm()
-        a.activate()
-        a.motionDetected()
-        a.reset()
-        a.motionDetected()
-        self.assertTrue(a.isRinging())
+        self.a.activate()
+        self.a.motionDetected()
+        self.a.reset()
+        self.a.motionDetected()
+        self.assertTrue(self.a.isRinging())
 
     def test_offAlarmDoesNotRingAfter_ThreeMotionDetections(self):
-        a = Alarm()
-        a.deactivate()
-        a.motionDetected()
-        a.motionDetected()
-        a.motionDetected()
-        self.assertFalse(a.isRinging())
+        self.a.deactivate()
+        self.a.motionDetected()
+        self.a.motionDetected()
+        self.a.motionDetected()
+        self.assertFalse(self.a.isRinging())
 
     def test_a_ringing_alarm_deactivated_does_not_ring_anymore_if_it_detects_motion(self):
-        a = Alarm()
-        a.activate()
-        a.motionDetected()
-        a.deactivate()
-        a.motionDetected()
-        self.assertFalse(a.isRinging())
-    
+        self.a.activate()
+        self.a.motionDetected()
+        self.a.deactivate()
+        self.a.motionDetected()
+        self.assertFalse(self.a.isRinging())
